88863d4c79758cab527146bccc922ec30d7c6e8b
Yooololo <85139209+Yooololo@users.noreply.github.com>
2022-01-12 20:01:34 -0300

    s

--- PI-Countries-main/client/src/components/Landing.jsx
+++ PI-Countries-main/client/src/components/Landing.jsx
@@ -1,22 +1,33 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
-import './Landing.css';
-import Tierra from '../components/Video/Tierra.mp4'; 
+import React from "react";
+import { Link } from "react-router-dom";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import "./Landing.css";
+import Tierra from "../components/Video/Tierra.mp4";
 
 export default function LandingPage() {
-    return (
-        <div className="landing-page">
-            <video id='videotierra' className='video' autoPlay loop muted>
-                <source src={Tierra} type="video/mp4"/>
-            </video>
-                <h1 className='titulo'>Welcome to planet Earth!</h1>
-                <p>
-                    <Link to="/home">
-                        <button className="homebtn">
-                            Access
-                        </button>
-                    </Link>
-                </p>
-        </div>
-    );
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getCountries());
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  useEffect(() => {
+    dispatch(getActivities());
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return (
+    <div className="landing-page">
+      <video id="videotierra" className="video" autoPlay loop muted>
+        <source src={Tierra} type="video/mp4" />
+      </video>
+      <h1 className="titulo">Welcome to planet Earth!</h1>
+      <p>
+        <Link to="/home">
+          <button className="homebtn">Access</button>
+        </Link>
+      </p>
+    </div>
+  );
 }
