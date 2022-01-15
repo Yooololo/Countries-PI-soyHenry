e2977ad4b03bfefb70d3b2b74419314ce25faaa7
Yooololo <85139209+Yooololo@users.noreply.github.com>
2022-01-14 22:22:25 -0300

    nowwedo

--- PI-Countries-main/api/index.js
+++ PI-Countries-main/api/index.js
@@ -19,52 +19,10 @@
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
-const { Country } = require("./src/db");
-const { Sequelize } = require("sequelize");
-const axios = require("axios");
-const dotenv = require("dotenv");
-dotenv.config();
-
-const country_activity = require("./src/forpsql/country_activity");
 
 // Syncing all the models at once.
 conn.sync({ force: false }).then(() => {
   server.listen(process.env.PORT, () => {
     console.log(`Listening on: PORT ${process.env.PORT}`); // eslint-disable-line no-console
-    const countriesAPI = "https://restcountries.com/v3.1/all";
-    const APIData = async (req, res) => {
-      const getAllData = await axios.get(countriesAPI);
-      const getData = getAllData.data;
-      getData.map((country) => {
-        return Country.create({
-          name: country.name.common.toString(),
-          image: country.flags.png.toString(),
-          continent: country.continents.toString(),
-          capital: country.capital ? country.capital.toString() : "",
-          id: country.cca3.toString(),
-          subregion: country.subregion,
-          area: country.area.toString(),
-          population: country.population.toString(),
-          gmapslink: country.maps.googleMaps.toString(),
-          timezones: country.timezones.toString(),
-          officialname: country.name.official.toString(),
-          currency: country.capital
-            ? Object.values(country.currencies)[0].name
-            : "",
-          currencysymbol: country.capital
-            ? Object.values(country.currencies)[0].symbol
-            : "",
-        })
-          .then(() => {
-            country_activity();
-          })
-          .catch((err) => console.log(err));
-      });
-    };
-    APIData()
-      .then(() => {
-        console.log("Data added correctly to DB");
-      })
-      .catch((err) => console.log(err));
   });
 });
--- PI-Countries-main/client/src/index.js
+++ PI-Countries-main/client/src/index.js
@@ -1,20 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import {Provider} from 'react-redux';
-import {store} from './store';
+import React from "react";
+import ReactDOM from "react-dom";
+import axios from "axios";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import reportWebVitals from "./reportWebVitals";
+import { Provider } from "react-redux";
+import { store } from "./store";
+import dotenv from "dotenv";
+dotenv.config();
 
+axios.defaults.baseURL = process.env.REACT_APP_API || "http://localhost:3001";
 
 ReactDOM.render(
-  <Provider store={store}>
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
-  </Provider>,
-  document.getElementById('root')
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
 
-
+serviceWorker.unregister();
 reportWebVitals();
